d1b33263c8924286455303609da7889b64e71d06
tekno <taperkinsbuchan404@gmail.com>
2022-07-01 13:19:52 -0400

    + Clipboard method added - allows user to clipboard generated password + Repositioned JLabel +Created Error Textbox +Password Length Longer

--- src/ActualCode.java
+++ src/ActualCode.java
@@ -1,11 +1,14 @@
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 
 public class ActualCode extends JFrame implements ActionListener {
-
+    ButtonGroup bg = new ButtonGroup();
     JButton gButton = new JButton("Generate");
     JTextField PasswordLen = new JTextField();
     JLabel Password = new JLabel("Password:");
@@ -16,12 +19,12 @@ public class ActualCode extends JFrame implements ActionListener {
     ActualCode(){
 
         frame.setTitle("Password-o Generator-o");
-        Question.setBounds(65,120,260,40);
+        Question.setBounds(65,25,260,40);
         gButton.setBounds(57,60,100,40);
         PasswordLen.setBounds(215,60,100,35);
         gButton.addActionListener(this);
 
-        ButtonGroup bg = new ButtonGroup();
+
         bg.add(gButton);
         frame.add(gButton);
         frame.add(PasswordLen);
@@ -32,6 +35,13 @@ public class ActualCode extends JFrame implements ActionListener {
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setResizable(false);
+
+
+
+        JLabel Clipped = new JLabel("Password copied to your clipboard");
+        Clipped.setBounds(69, 90, 260, 40);
+        frame.add(Clipped);
+        Clipped.setText("Password Will to your clipboard");
     }
 
     private String ActualLogic(int theSize){
@@ -48,29 +58,60 @@ public class ActualCode extends JFrame implements ActionListener {
         return sb.toString();
     }
 
+
+
+    void copyFucntion(String copy){
+
+
+        StringSelection copyThis = new StringSelection(copy);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(copyThis, null);
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
         String theLength = PasswordLen.getText();
         int PasswordLength = Integer.parseInt(theLength);
 
-    if (PasswordLength < 9 || PasswordLength >= 18) {
+        if (PasswordLength < 9 || PasswordLength > 30) {
 
-        System.out.println("Error: Password needs to stay at least within the 9- 18 range of characters please try again");
-        JTextField finishedProduct = new JTextField("Error: Please keep it between 9 - 18 characters long");
-        finishedProduct.setBounds(65, 120, 260, 40);
-        finishedProduct.setEditable(false);
-        frame.add(finishedProduct);
+            System.out.println("");
+            JTextField Error = new JTextField();
+            Error.setBounds(65, 120, 260, 40);
+            Error.setEditable(false);
+            frame.add(Error);
+            Error.setText("Error: Please keep it between 9 - 30+ characters long");
+            Error.revalidate();
+
+
+
+
+        }
+
+
+        else {
+
+            JTextField finishedProduct = new JTextField();
+            finishedProduct.setBounds(65, 120, 260, 40);
+            finishedProduct.setEditable(false);
+
+            var PasswordDone = ActualLogic(PasswordLength);
+            finishedProduct.setText(PasswordDone);
+
+            frame.add(finishedProduct);
+            copyFucntion(PasswordDone);
+
+
+
+        }
+
+
     }
-    if (gButton.isEnabled()) {
 
-        JTextField finishedProduct = new JTextField(ActualLogic(PasswordLength));
-        finishedProduct.setBounds(65, 120, 260, 40);
-        finishedProduct.setEditable(false);
 
-        frame.add(finishedProduct);
 
-    }
-    }
 
 }
+
